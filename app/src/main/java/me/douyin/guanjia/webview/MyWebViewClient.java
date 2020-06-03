d1201a313c754d13926a46ad5fae8ec065191994
liliangxing <253254457@qq.com>
2020-06-03 18:27:14 +0800

    1

--- app/src/main/java/me/douyin/guanjia/webview/MyWebViewClient.java
+++ app/src/main/java/me/douyin/guanjia/webview/MyWebViewClient.java
@@ -52,7 +52,8 @@ public class MyWebViewClient extends WebViewClient {
                     "document.getElementsByTagName('html')[0].innerHTML+'</head>');");
         }
         if(url.startsWith("https://h5.weishi.qq.com/weishi/feed/")
-        ||url.startsWith("https://www.iesdouyin.com/share/video/")){
+        ||url.startsWith("https://www.iesdouyin.com/share/video/")
+        ||url.contains("https://www.douyin.com/share/forward/")){
             view.loadUrl("javascript:setTimeout(function () {window.java_obj.getSource('<head>'+" +
                     "document.getElementsByTagName('html')[0].innerHTML+'</head>');},2000);");
         }
